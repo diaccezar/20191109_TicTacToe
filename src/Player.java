c6c186212ffd1cbd847d85bc0bdc00069e43b82f
Cezar Diac <diaccezar@yahoo.com>
2019-11-09 12:02:11 +0200

    initial commit

---
+++ src/Player.java
@@ -0,0 +1,9 @@
+public class Player {
+    public String name;
+    public char symbol;
+
+    public Player(String name, char symbol) {
+        this.name = name;
+        this.symbol = symbol;
+    }
+}
---
+++ src/TicTacToe.java
@@ -0,0 +1,48 @@
+import javax.imageio.metadata.IIOMetadataFormatImpl;
+
+public class TicTacToe {
+
+    public static final char SYMBOL_X = 'X';
+    public static final char SYMBOL_0 = '0';
+    public static final int SIZE = 3;   // STATIC SCOPE
+
+    public char[][] board = new char[SIZE][SIZE];  // INSTANCE SCOOPE
+
+    public Player player1;
+    public Player player2;
+
+    public TicTacToe(Player player1, Player player2) {
+        this.player1 = player1;
+        this.player2 = player2;
+    }
+
+    public void initBoard() {
+        // puts spaces into all locations on board
+        for (int i = 0; i < SIZE; i++) {
+            for (int j = 0; j < SIZE; j++) {
+                board[i][j] = ' ';
+            }
+        }
+    }
+
+    // shows the current state of the game
+    public void showBoard() {
+        System.out.println("Board:");
+
+        for (int i = 0; i < SIZE; i++) {
+            for (int j = 0; j < SIZE; j++) {
+                int x = i * SIZE + j + 1;
+                System.out.print("|_" + x + "_| ");
+            }
+            System.out.println();
+
+        }
+    }
+
+    public void playGame() {
+        //init board
+        initBoard();
+        showBoard();
+
+    }
+}
